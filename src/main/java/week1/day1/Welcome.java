fe0718d210289e430d77b99eb1a309d306f46194
admin <vidyar1926@gmail.com>
2023-11-23 18:12:50 +0530

    newcommit

---
+++ src/main/java/week1/day1/Welcome.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+import week1.day2.LearnMethods;
+
+public class Welcome {
+	
+	
+	
+	
+	//type main -->ctrl space enter
+	//entry point for execution/run the code/logic
+	public static void main(String[] args) {
+		
+		//print a statement
+		//syso ctrl space enter
+		System.out.println("Welcome to Testleaf");
+		LearnMethods sum=new LearnMethods();
+		sum.add(10, 30);
+		
+		
+	}
+	
+
+}
